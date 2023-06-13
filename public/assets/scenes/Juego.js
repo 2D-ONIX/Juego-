1a8f9f1a43292029e04596b3ec84e4b3a4052d1f
2D-ONIX <tomiterran@gmail.com>
2023-06-13 19:13:04 -0300

    El juego v0.1

--- public/assets/scenes/Juego.js
+++ public/assets/scenes/Juego.js
@@ -2,153 +2,199 @@
 
 export default class Juego extends Phaser.Scene {
   constructor() {
-    // key of the scene
-    // the key will be used to start the scene by other scenes
     super("hello-world");
   }
 
   init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
+    // ...
   }
 
+  text;
+  cursors;
+  sprite;
+  enemies;
+  bullets;
+  lives;
+  livesText;
+  points;
+  pointsText;
+  enemyCount;
+  enemyTimer;
+  spaceKey;
+  asteroids;
+  activeEnemiesText;
+
   preload() {
-    // load assets
-    this.load.tilemapTiledJSON("map", "./public/tilemaps/nivel1.json");
-    this.load.image("tilesFondo", "./public/assets/images/sky.png");
-    this.load.image("tilesPlataforma", "./public/assets/images/platform.png");
-
-    this.load.image("star", "./public/assets/images/star.png");
-
-    this.load.spritesheet("dude", "./public/assets/images/dude.png", {
-      frameWidth: 32,
-      frameHeight: 48,
-    });
+    this.load.image('mybullet', 'public/assets/images/mybullet.png');
+    this.load.image('enemybullet', 'public/assets/images/enemybullet.png');
+    this.load.image('ship', 'public/assets/images/ship.png');
+    this.load.image('enemy', 'public/assets/images/enemy.png');
+    this.load.image('asteroid', 'assets/games/asteroids/asteroid.png');
   }
 
   create() {
-    // todo / para hacer: texto de puntaje
+    this.sprite = this.physics.add.image(400, 300, 'ship');
 
-    //  Our player animations, turning, walking left and walking right.
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
+    this.sprite.setDamping(true);
+    this.sprite.setDrag(0.99);
+    this.sprite.setMaxVelocity(200);
 
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    const map = this.make.tilemap({ key: "map" });
-
-    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
-    // Phaser's cache (i.e. the name you used in preload)
-    const capaFondo = map.addTilesetImage("sky", "tilesFondo");
-    const capaPlataformas = map.addTilesetImage("platform", "tilesPlataforma");
-
-    // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const fondoLayer = map.createLayer("fondo", capaFondo, 0, 0);
-    const plataformaLayer = map.createLayer(
-      "plataformas",
-      capaPlataformas,
-      0,
-      0
-    );
-    const objectosLayer = map.getObjectLayer("objetos");
-
-    plataformaLayer.setCollisionByProperty({ colision: true });
-
-    console.log(objectosLayer);
-
-    // crear el jugador
-    // Find in the Object Layer, the name "dude" and get position
-    const spawnPoint = map.findObject(
-      "objetos",
-      (obj) => obj.name === "jugador"
-    );
-    console.log(spawnPoint);
-    // The player and its settings
-    this.jugador = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "dude");
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    this.jugador.setBounce(0.1);
-    this.jugador.setCollideWorldBounds(true);
-
-    //  Input Events
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-    // Create empty group of starts
-    this.estrellas = this.physics.add.group();
+    this.text = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
+    this.lives = 3;
+    this.livesText = this.add.text(10, 40, 'Lives: 3', { font: '16px Courier', fill: '#ff0000' });
+    this.points = 0;
+    this.pointsText = this.add.text(10, 70, 'Points: 0', { font: '16px Courier', fill: '#ffffff' });
 
-    // find object layer
-    // if type is "stars", add to stars group
-    objectosLayer.objects.forEach((objData) => {
-      //console.log(objData.name, objData.type, objData.x, objData.y);
+    this.enemies = this.physics.add.group();
+    this.bullets = this.physics.add.group();
+    this.asteroids = this.physics.add.group();
 
-      const { x = 0, y = 0, name } = objData;
-      switch (name) {
-        case "estrella": {
-          // add star to scene
-          // console.log("estrella agregada: ", x, y);
-          const star = this.estrellas.create(x, y, "star");
-          break;
-        }
-      }
-    });
+    this.enemyCount = 0;
+    this.enemyTimer = this.time.addEvent({ delay: 10000, callback: this.spawnEnemy, callbackScope: this, repeat: 9 });
 
-    this.physics.add.collider(this.jugador, plataformaLayer);
-    this.physics.add.collider(this.estrellas, plataformaLayer);
-    this.physics.add.collider(
-      this.jugador,
-      this.estrellas,
-      this.recolectarEstrella
-    );
+    this.time.addEvent({ delay: 5000, callback: this.enemyShoot, callbackScope: this, loop: true });
+
+    this.physics.add.collider(this.bullets, this.enemies, this.hitEnemy, this.checkPlayerBulletCollision, this);
+    this.physics.add.collider(this.bullets, this.asteroids, this.hitAsteroid, null, this);
+    this.physics.add.collider(this.enemies, this.sprite, this.hitEnemyPlayer, null, this);
+    this.physics.add.collider(this.asteroids, this.sprite, this.hitAsteroidPlayer, null, this);
+    this.physics.add.collider(this.sprite, this.bullets, this.hitPlayerBullet, this.checkEnemyBulletCollision, this);
+
+    this.physics.world.setBoundsCollision(true, true, true, true);
   }
 
   update() {
-    // update game objects
-    // check input
-    //move left
-    if (this.cursors.left.isDown) {
-      this.jugador.setVelocityX(-160);
-      this.jugador.anims.play("left", true);
-    }
-    //move right
-    else if (this.cursors.right.isDown) {
-      this.jugador.setVelocityX(160);
-      this.jugador.anims.play("right", true);
-    }
-    //stop
-    else {
-      this.jugador.setVelocityX(0);
-      this.jugador.anims.play("turn");
+    if (this.cursors.up.isDown) {
+      this.physics.velocityFromRotation(this.sprite.rotation, 200, this.sprite.body.acceleration);
+    } else {
+      this.sprite.setAcceleration(0);
     }
 
-    //jump
-    if (this.cursors.up.isDown && this.jugador.body.blocked.down) {
-      this.jugador.setVelocityY(-330);
+    if (this.cursors.left.isDown) {
+      this.sprite.setAngularVelocity(-300);
+    } else if (this.cursors.right.isDown) {
+      this.sprite.setAngularVelocity(300);
+    } else {
+      this.sprite.setAngularVelocity(0);
+    }
+
+    this.text.setText(`Speed: ${this.sprite.body.speed}`);
+
+    this.physics.world.wrap(this.sprite, 32);
+
+    this.enemies.children.each((enemy) => {
+      this.enemyMove(enemy);
+      this.physics.world.wrap(enemy, 16);
+    });
+
+    if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+      this.playerShoot();
     }
   }
 
-  recolectarEstrella(jugador, estrella) {
-    estrella.disableBody(true, true);
+  spawnEnemy() {
+    if (this.enemyCount < 10) {
+      const enemy = this.physics.add.sprite(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 500), 'enemy');
+      this.enemies.add(enemy);
+      this.enemyCount++;
 
-    // todo / para hacer: sumar puntaje
+      const randomAngle = Phaser.Math.FloatBetween(-Math.PI, Math.PI);
+      enemy.rotation = randomAngle;
+    }
+  }
 
-    // todo / para hacer: controlar si el grupo esta vacio
-    // todo / para hacer: ganar el juego
+  enemyMove(enemy) {
+    this.physics.velocityFromRotation(enemy.rotation, 100, enemy.body.velocity);
+  }
+
+  enemyShoot() {
+    this.enemies.children.each((enemy) => {
+      if (!enemy.getData('isDestroyed')) {
+        const bullet = this.bullets.create(enemy.x, enemy.y, 'enemybullet');
+        bullet.rotation = this.physics.accelerateToObject(bullet, this.sprite, 100);
+        this.physics.velocityFromRotation(bullet.rotation, 200, bullet.body.velocity);
+      }
+    });
+  }
+
+  playerShoot() {
+    const bullet = this.bullets.create(this.sprite.x, this.sprite.y, 'mybullet');
+    bullet.rotation = this.sprite.rotation;
+    this.physics.velocityFromRotation(bullet.rotation, 200, bullet.body.velocity);
+  }
+
+  hitEnemy(bullet, enemy) {
+    bullet.disableBody(true, true);
+    enemy.disableBody(true, true);
+    enemy.setData('isDestroyed', true);
+    this.enemyCount++;
+    this.points += 50;
+    this.pointsText.setText(`Points: ${this.points}`);
+
+    if (this.enemyCount === 0) {
+      this.enemyTimer.paused = true;
+    }
+  }
+
+  hitEnemyPlayer(player, enemy) {
+    enemy.disableBody(true, true);
+    this.lives--;
+    this.livesText.setText(`Lives: ${this.lives}`);
+
+    if (this.lives <= 0) {
+      this.playerDestroyed();
+    }
+  }
+
+  hitAsteroid(asteroid, bullet) {
+    bullet.disableBody(true, true);
+    asteroid.disableBody(true, true);
+    this.points += 20;
+    this.pointsText.setText(`Points: ${this.points}`);
+  }
+
+  hitAsteroidPlayer(asteroid, player) {
+    asteroid.disableBody(true, true);
+    this.lives--;
+    this.livesText.setText(`Lives: ${this.lives}`);
+
+    if (this.lives <= 0) {
+      this.playerDestroyed();
+    }
+  }
+
+  hitPlayerBullet(player, bullet) {
+    bullet.disableBody(true, true);
+    this.lives--;
+    this.livesText.setText(`Lives: ${this.lives}`);
+
+    if (this.lives <= 0) {
+      this.playerDestroyed();
+    }
+  }
+
+  checkPlayerBulletCollision(bullet, enemy) {
+    return bullet.texture.key === 'mybullet';
+  }
+
+  checkEnemyBulletCollision(player, bullet) {
+    return bullet.texture.key === 'enemybullet';
+  }
+
+  playerDestroyed() {
+    this.sprite.disableBody(true, true);
+    this.livesText.setText('Game Over');
+    this.time.addEvent({ delay: 3000, callback: this.restartGame, callbackScope: this });
+  }
+
+  restartGame() {
+    this.scene.restart();
   }
 }
+
+
+
+
